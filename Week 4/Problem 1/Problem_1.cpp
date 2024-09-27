fccc715067e28f20ffccff8ded93d9519652fa45
Shadiq Aryasatya <skulltrooperfortnite2017@gmail.com>
2024-09-27 15:35:50 +0700

    letters pluh

--- Week 4/Problem 1/Problem_1.cpp
+++ Week 4/Problem 1/Problem_1.cpp
@@ -2,31 +2,31 @@
 using namespace std;
 
 int main (){
-    int amount_of_real_numbers_summed_up;
-    int number_in_the_loop;
-    int sum_of_real_numbers;
+    int i;
+    int j;
+    int k;
 
-    number_in_the_loop = 1;
-    sum_of_real_numbers = 0;
+   j = 1;
+    k = 0;
 
     cout<<"Please input amount of natural numbers that will be summed up."<<endl;
-    cin>>amount_of_real_numbers_summed_up;
+    cin>>i;
 
     cout<<'\n';
 
-    cout<<"Input = "<<amount_of_real_numbers_summed_up<<'\n';
+    cout<<"Input = "<<i<<'\n';
 
     cout<<"Output = ";
 
  
-    while(number_in_the_loop <= amount_of_real_numbers_summed_up){
-        cout<<number_in_the_loop;
-        sum_of_real_numbers += number_in_the_loop;
-        number_in_the_loop++;
-        if(number_in_the_loop <= amount_of_real_numbers_summed_up){
+    while(j <= i){
+        cout<<j;
+        k += j;
+        j++;
+        if(j <= i){
             cout<<" + ";
         }
     }
     
-    cout<<" = "<<sum_of_real_numbers<<'\n';
+    cout<<" = "<<k<<'\n';
 }
